0c97e845ff34cc97827480a0fe97246371b4e4ec
mahdi.davaeiha <m.m.davaeiha@gmail.com>
2022-08-16 17:38:48 +0430

    clean up some comments inside code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from "react";
 import Editor from "./components/Editor";
-// import Menu from "./components/Menu.tsx";
-// import { MoreVertSVGIcon } from "@react-md/material-icons";
-// import { Configuration } from "@react-md/layout";
 
 function App() {
   return (
     <>
       <Editor />
-      {/* <Configuration>
-        <Menu
-          // id={`fab-menu-${position}`}
-          // key={position}
-          aria-label="Options..."
-          // floating={position}
-          buttonChildren={<MoreVertSVGIcon />}
-          fixedPositionOptions={{
-            disableSwapping: true,
-          }}
-        />
-      </Configuration> */}
     </>
   );
 }
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,55 +1,14 @@
-import React, { useState ,useContext} from "react";
-import {
-    FloatingMenu,
-    EditorContent,
-    useEditor,
-    BubbleMenu
-  } from '@tiptap/react';
-import StarterKit from '@tiptap/starter-kit';
-import DropdownMenu from "./DropdownMenu";
-import Node from "./Extension.js";
+import React, {useContext} from "react";
+import { EditorContent} from '@tiptap/react';
+
 import { EditorContext } from "../contexts/EditorContext";
 
 
 const Editor = () => {
   const editor = useContext(EditorContext);
 
-  // const {menu} = useGlobalContext();
-  // const editor = useEditor({
-  //     extensions: [
-  //       StarterKit,
-  //       Node
-  //     ],
-  //     content: `
-  //     <p>This is a boring paragraph.</p>
-  //     <div data-type="draggable-item">
-  //       <p>Followed by a fancy draggable item.</p>
-  //     </div>
-  //     <div data-type="draggable-item">
-  //       <p>And another draggable item.</p>
-  //       <div data-type="draggable-item">
-  //         <p>And a nested one.</p>
-  //         <div data-type="draggable-item">
-  //           <p>But can we go deeper?</p>
-  //         </div>
-  //       </div>
-  //     </div>
-  //     <p>Let’s finish with a boring paragraph.</p>   
-  //     <draggableItem toggleDropDown={toggleDropDown}><p></p></draggableItem>
-  //     `,
-  //   })
-
-    
   return (
     <>       
-      {/* {
-        editor && <BubbleMenu tippyOptions={{ duration: 100 }} editor={editor}>
-            <DropdownMenu editor={editor} />
-        </BubbleMenu>
-      }     */}
-
-      {/* {(menu && editor) ? <DropdownMenu editor={editor} /> : null} */}
-
       <EditorContent editor={editor} className="editor"/>
     </>
   )
